efe1904570f29f78258ff2fdc9f6e89e3697ab33
Ardie Wen <ardie.wen@hunterlily.co>
2021-05-31 19:35:01 +0000

    feat: repeat static image

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,226 @@ const IndexPage = () => (
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+      nunc nec magna lobortis feugiat. Praesent orci enim, convallis eget odio
+      vel, imperdiet consequat enim. Sed tempor interdum ipsum at interdum.
+      Suspendisse at fermentum ipsum. Ut eget fringilla elit. Aliquam porta,
+      ligula vitae finibus lobortis, urna dolor laoreet ligula, in hendrerit
+      tellus augue a purus. Donec enim lectus, lobortis ut felis imperdiet,
+      dapibus varius diam. Proin auctor lobortis sem vitae commodo. Nam ut
+      tempor nulla. Cras molestie neque vitae quam sollicitudin convallis.
+      Vestibulum finibus massa mattis mi ultrices, sit amet tincidunt mi
+      volutpat. Aenean at venenatis ligula. Mauris eget ipsum nec metus laoreet
+      bibendum non ac leo. Etiam tortor urna, ornare vitae sodales consectetur,
+      posuere a sapien.
+    </p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
   </Layout>
 )
 
